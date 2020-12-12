e2a34dbd12854ba8d911f896a6386b460ee945af
PsychoMantisAllen <alenyim0403@hotmail.com>
2020-12-13 10:16:55 +1100

    Third time commit for regex learning

--- Regex Intro.py
+++ Regex Intro.py
@@ -22,7 +22,7 @@ print(isPhoneNumber('415-555-1234'))
 message = 'Call me 415-555-1011 tomorrow, or at 415-555-9999 for my office number'
 foundNumber = False
 for i in range(len(message)):
-    chunk = message[i:i+12]
+    chunk = message[i:i + 12]
     if isPhoneNumber(chunk):
         print('Phone number found: ' + chunk)
         foundNumber = True
@@ -31,6 +31,7 @@ if not foundNumber:
 
 # this would be a normal way to code and find the phone numbers. However, it would be an easier case if using regex
 import re
+
 message = 'Call me 415-555-1011 tomorrow, or at 415-555-9999 for my office number'
 phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
 mo = phoneNumRegex.search(message)
@@ -90,5 +91,67 @@ print(digitRegex.search('1234567890'))
 # if we want to do a non-greedy match, we just need to put a question mark behind
 digitRegex = re.compile(r'(\d){3,5}?')
 
+# remember that we use findall() method to return a list of strings that meets the criteria
+# if we structure a bit more about the compile function, we will get tuples
+resume = '123-321-1234 and what is another one again? 392-123-9484'
+phoneRegex = re.compile(r'(\d\d\d)-(\d\d\d-\d\d\d\d)')
+phoneRegex.findall(resume)
 
+# now try this one!
+phoneRegex = re.compile(r'((\d\d\d)-(\d\d\d-\d\d\d\d))')
+phoneRegex.findall(resume)
+
+# \d - any numeric digit from 0 to 9
+# \D - any character that is not a numeric digit from 0 to 9
+# \w - any letter, numeric digit, or the underscore character (think of this as matching 'word' characters)
+# \W - any character that is not a letter, numeric digit, or the underscore character
+# \s - any space, tab, or newline character (think of this as matching 'space' characters)
+# \S - any character that is not a space, tab or newline character
+
+vowelRegex = re.compile(r'[aeiouAEIOU]')  # same as r'(a|e|i|o|u|A|E|I|O|U)'
+print(vowelRegex.findall('Robocop eats baby food'))
+# if we want two vowels in a row
+vowelRegex = re.compile(r'[aeiouAEIOU]{2}')
+print(vowelRegex.findall('Robocop eats baby food'))
+# if we want all non-vowel letters, simply just put a carrot sign
+vowelRegex = re.compile(r'[^aeiouAEIOU]')
+print(vowelRegex.findall('Robocop eats baby food'))
+
+# starting with something or end with something in Regex
+beginsWithHelloRegex = re.compile(r'^Hello')
+endsWithWorldRegex = re.compile(r'world!$')
+# if the text does not start with Hello or end with world! then it will return a None
+# using both we can find the exact pattern in a string
+allDigitsRegex = re.compile(r'^/d+$')
+
+# adding a dot can represent any characters except for the new line
+# it only looks for a single character
+atRegex = re.compile(r'.at')
+print(atRegex.findall('The cat in the hat sat on the flat mat. At haha'))
+# see here flat and at are not included, only lat for flat
+atRegex = re.compile(r'.{1,2}at')
+print(atRegex.findall('The cat in the hat sat on the flat mat. At haha'))
+# this one can help with the flat, but we can see for others a space is included, not ideal
+
+# (.*) finds anything after that, except for new line
+nameRegex = re.compile(r'First Name: (.*) Last Name:(.*)')
+print(nameRegex.findall('First Name: Allen Last Name: Yan'))
+# returning a tuple of strings, see here if we change Last Name to without upper case, we will get a NONE
+
+# to keep in mind, (.*) is greedy and (.*?) is non-greedy
+serve = '<To serve humans> for dinner.>'
+nonGreedy = re.compile(r'<(.*?)>')
+print(nonGreedy.findall(serve))
+isGreedy = re.compile(r'<(.*)>')
+print(isGreedy.findall(serve))
+
+# now let's see what except for new line means
+prime = 'I take over the next arcade.\nWhat could the world have done to compromise me\nThat would be the abysmal.'
+dotStar = re.compile(r'.*')
+print(dotStar.search(prime))
+# if we want everything including new lines
+dotStar = re.compile(r'.*', re.DOTALL)
+print(dotStar.search(prime))
+# similarly we can have case insensitive while searching
+vowelRegex = re.compile(r'[aeiou]', re.I)
 
