75b18e9af54d56adc5b0b92539d061471dca07f7
PsychoMantisAllen <alenyim0403@hotmail.com>
2020-12-16 22:38:55 +1100

    Regex Find Phone and Email Example

--- PhoneAndEmailRegex.py
+++ PhoneAndEmailRegex.py
@@ -14,6 +14,7 @@ phoneRegex = re.compile(r'''
 (\d{2,5}))?                 # extension number-part (optional)
 
 ''', re.VERBOSE)
+
 # Create a regex for email addresses
 emailRegex = re.compile(r'''
 # sample some.+_thing@some.+_thing.com/edu etc
@@ -37,7 +38,6 @@ print(extractedEmail)
 # if we run the result here we will see phone is in tuple since it is constructed by groups
 # while email is only one group
 # to solve this, simply just add parenthesis to make it as one group
-
 phoneRegex = re.compile(r'''
 # sample 415-555-0000, 555-0000, (415) 555-0000, 555-0000 ext 12345, ext. 12345, x12345
 
@@ -55,7 +55,6 @@ phoneRegex = re.compile(r'''
 
 # and since the phone number we actually want is the first item in the tuple
 # we need to only extract a group of it
-
 allPhoneNumbers = []
 for phoneNumber in extractedPhone:
     allPhoneNumbers.append(phoneNumber[0])
@@ -65,7 +64,6 @@ print(allPhoneNumbers)
 # Copy the extracted email/phone to the clipboard
 # since the results are in a list format and there is quote mark
 # we want to have pure phone number and email followed line-by-line
-
 results = '\n'.join(allPhoneNumbers) + '\n' + '\n'.join(extractedEmail)
 pyperclip.copy(results)
 
